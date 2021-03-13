bf6dfce1657b3273a299b255419ede70166be0f9
KyungMin_ <tommykmin@naver.com>
2021-03-14 01:29:33 +0900

    [UPDATE] day01 class05

--- src/main/java/day01/class05/AppleSpeaker.java
+++ src/main/java/day01/class05/AppleSpeaker.java
@@ -2,7 +2,7 @@ package day01.class05;
 
 import org.springframework.stereotype.Component;
 
-@Component("apple")//SonySpeaker에도 @Component로 객체가 모두 메모리에 생성되어 있다면 컨테이너는 어떤 객체를 
+//@Component("apple")//SonySpeaker에도 @Component로 객체가 모두 메모리에 생성되어 있다면 컨테이너는 어떤 객체를 
 				//할당할지 스스로 판단할 수 없어서 에러가 발생
 public class AppleSpeaker implements Speaker {	
 
--- src/main/java/day01/class05/SonySpeaker.java
+++ src/main/java/day01/class05/SonySpeaker.java
@@ -2,7 +2,7 @@ package day01.class05;
 
 import org.springframework.stereotype.Component;
 
-@Component("sony")//SonySpeaker객체가 메모리에 생성이 되어 있어야 하므로 
+//@Component("sony")//SonySpeaker객체가 메모리에 생성이 되어 있어야 하므로 
 public class SonySpeaker implements Speaker{
 
 	public SonySpeaker() {
